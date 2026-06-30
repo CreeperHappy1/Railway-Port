5e7390e84c82532ef32cfea6029da0101f8a6b86
CreeperHappy1 <jorgejuan.azpeitia.pozo@gmail.com>
2026-06-30 02:35:31 +0200

    small argument fix

--- src/main/java/com/railwayteam/railways/compat/tracks/mods/TFCTrackCompat.java
+++ src/main/java/com/railwayteam/railways/compat/tracks/mods/TFCTrackCompat.java
@@ -37,8 +37,8 @@ public class TFCTrackCompat extends GenericTrackCompat {
     @Override
     protected Ingredient getIngredientForRail() {
         return Ingredient.fromValues(Stream.of(
-                AccessorIngredient$TagValue.railways$create(TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("forge", "rods/zinc")))
                 AccessorIngredient$TagValue.railways$create(TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("forge", "rods/wrought_iron"))),
+                AccessorIngredient$TagValue.railways$create(TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("forge", "rods/zinc")))
         ));
     }
 
